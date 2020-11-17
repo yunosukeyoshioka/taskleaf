de87791b6d616db3a7caaa150ba0cd33ead5ae52
vagrant <vagrant@localhost.localdomain>
2020-11-17 23:31:29 +0000

    login

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+  def index
+  	@tasks = Task.all
+  end
+
+  def show
+  	@task = Task.find(params[:id])
+  end
+
+  def new
+  	@task = Task.new
+  end
+
+  def edit
+  	@task = Task.find(params[:id])
+  end
+
+  def create
+  	@task = Task.new(task_params)
+
+  	if @task.save
+  	 redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました"
+    else
+      render :new
+    end
+  	#flash[:notice] = "タスク「#{task.name}を登録しました」"
+  	#redirect_to tasks_url
+  end
+
+  def update
+  	task = Task.find(params[:id])
+  	task.update!(task_params)
+  	redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました"
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました"
+  end
+
+  private
+  	def task_params
+  		params.require(:task).permit(:name, :description)
+  	end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_081719) do
+ActiveRecord::Schema.define(version: 2020_11_17_093602) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
